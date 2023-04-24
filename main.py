c5057f8b64dc6b6704bf029f70560327eb466c3e
pyadrus <zh.vitaliy92@gmail.com>
2023-04-24 20:27:57 +0300

    Добавлены рабочие дни и норма часов в Июне 2023

--- main.py
+++ main.py
@@ -27,9 +27,8 @@ async def start_command(message: types.Message):
     """Обрабатываем команду /start"""
     main_keyboard = InlineKeyboardMarkup()
     raport_button = InlineKeyboardButton(text='🔨Рапорта 2023', callback_data='rap')
-    main_keyboard.row(raport_button)
     days_off_button = InlineKeyboardButton(text='📅 Выходные дни 2023', callback_data='days_off')
-    main_keyboard.row(days_off_button)
+    main_keyboard.row(raport_button, days_off_button)
     await message.reply("Выберите пункт:", reply_markup=main_keyboard)
 
 
@@ -37,12 +36,14 @@ async def start_command(message: types.Message):
 async def days_off_callback_month(callback_query: types.CallbackQuery):
     """Плановые и выходные дни в Мае 2023"""
     keyboard = InlineKeyboardMarkup()
-    mar_button = InlineKeyboardButton(text='📅 Май 2023', callback_data='mar_days_off')
-    keyboard.row(mar_button)
+    may_button = InlineKeyboardButton(text='📅 Май 2023', callback_data='may_days_off')
+    june_button = InlineKeyboardButton(text='📅 Июнь 2023', callback_data='june_days_off')
+    keyboard.row(may_button, june_button)
+
     await bot.send_message(callback_query.from_user.id, "📅 Выберите месяц:", reply_markup=keyboard)
 
 
-@dp.callback_query_handler(lambda c: c.data in ['mar_days_off'])
+@dp.callback_query_handler(lambda c: c.data in ['may_days_off'])
 async def days_off_process_callback_monthh(callback_query: types.CallbackQuery, state: FSMContext):
     selected_month = callback_query.data
     await state.update_data(month=selected_month)
@@ -59,6 +60,30 @@ async def days_off_process_callback_monthh(callback_query: types.CallbackQuery, 
                                   f"<code>📅 Праздничные:</code><b> 1, 9, 11</b>\n"
                                   f"\n<code>📅 Выходные поверхность:</code><b> 1, 6, 7, 8, 9, 10, 13, 14, 20, 21, 27, 28</b>\n"
                                   f"<code>📅 Выходные подземные:</code><b>1, 3, 7, 8, 9, 11, 14, 17, 21, 24, 28</b>\n"
+                                  f"\n<code>🔨 Выходов для 12 часовых:</code><b> {norm_hours_12h_shift}</b>\n"
+                                  f"<code>🔨 Выходов для 24 часовых:</code><b> {norm_hours_24h_shift}</b>\n"
+                                  f"\nНажмите /start, чтобы вернуться в начало", parse_mode="HTML")
+
+    await state.finish()
+
+
+@dp.callback_query_handler(lambda c: c.data in ['june_days_off'])
+async def days_off_process_callback_monthh(callback_query: types.CallbackQuery, state: FSMContext):
+    selected_month = callback_query.data
+    await state.update_data(month=selected_month)
+    await Form.district.set()
+
+    norm_working_days = 20
+    norm_working_hours = norm_working_days * 8
+    norm_hours_12h_shift = math.ceil(norm_working_hours / 12)
+    norm_hours_24h_shift = math.ceil(norm_working_hours / 24)
+
+    response_message = callback_query.message
+    await response_message.answer(f"<code>✅ Норма выходов в Июне 2023:</code><b> {norm_working_days}</b>\n"
+                                  f"\n<code>📅 Выходные:</code><b> 3, 5, 10, 11, 17, 18, 24, 25</b>\n"
+                                  f"<code>📅 Праздничные:</code><b> 4, 12</b>\n"
+                                  f"\n<code>📅 Выходные поверхность:</code><b> 3, 4, 5, 10, 11, 12, 17, 18, 24, 25</b>\n"
+                                  f"<code>📅 Выходные подземные:</code><b>4, 5, 7, 11, 12, 14, 18, 21, 25, 28</b>\n"
                                   f"\n<code>🔨 Выходов для 12 часовых:</code><b> {norm_hours_12h_shift}</b>\n"
                                   f"<code>🔨 Выходов для 24 часовых:</code><b> {norm_hours_24h_shift}</b>\n"
                                   f"\nНажмите /start, чтобы вернуться в начало", parse_mode="HTML")
@@ -97,9 +122,8 @@ async def process_district(message: types.Message, state: FSMContext):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
     # Создаем таблицу, если ее нет
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS user_requests (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER,
-                                                  username TEXT, timestamp TEXT, file_name TEXT)""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS user_requests (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER,
+                                                                username TEXT, timestamp TEXT, file_name TEXT)""")
     # Добавляем запись в таблицу
     cursor.execute("""INSERT INTO user_requests (user_id, username, timestamp, file_name) 
                       VALUES (?, ?, ?, ?)""", (user_id, username, timestamp, file_name))
