20e897a9abaf27a60a10e89cc0b6dae067328225
pyadminrus <zh.vitaliy92@gmail.com>
2024-02-16 17:24:29 +0300

    Оптимизация кода

--- handlers/days_off_handlers_2022.py
+++ handlers/days_off_handlers_2022.py
@@ -6,9 +6,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from keyboards.welcome_keyboard import keyboard_go_back
 from system.global_variables import *
-# from system.global_variables_2022 import *
 from system.system import dp, bot
-import json
 
 
 @dp.callback_query_handler(lambda c: c.data in ['days_off_22'])
