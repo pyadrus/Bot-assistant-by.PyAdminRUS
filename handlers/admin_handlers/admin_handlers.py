dbd75127ca1aa347cf0e1d80fe7fcf5562330fa3
@PyAdminRU <zh.vitaliy92@gmail.com>
2025-04-22 18:04:21 +0300

    Добавлена проверка на наличие сформированных табелей 2024 года. Рефакторинг кода.

--- handlers/admin_handlers/admin_handlers.py
+++ handlers/admin_handlers/admin_handlers.py
@@ -9,6 +9,32 @@ from loguru import logger
 
 from system.system import dp, bot, router
 
+@router.callback_query(F.data == "checking_availability_of_tab")
+async def checking_availability_of_tab(callback_query: types.CallbackQuery):
+    """Проверка наличия табеля"""
+    # Путь к папке с файлами
+    folder_dic = ["01_jan_tab_2024", "02_feb_tab_2024", "03_mar_tab_2024", "04_apr_tab_2024", "05_may_tab_2024",
+                  "06_jun_tab_2024", "07_jul_tab_2024", "08_aug_tab_2024", "09_sep_tab_2024", "10_oct_tab_2024",
+                  "11_nov_tab_2024", "12_dec_tab_2024"]
+    for dir in folder_dic:
+        logger.info(dir)
+        with open(f'report_card/tab_2024/{dir}/tab_2024.json', 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            logger.info(data)
+        # Создаем текстовый файл для записи отсутствующих файлов
+        missing_files = []
+        with open(f"отсутствующие_файлы_{dir}.txt", "w", encoding='utf-8') as file:
+            for code, description in data.items():
+                filename = f"{code}.xls"
+                if filename not in os.listdir(f"report_card/tab_2024/{dir}"):
+                    missing_files.append(filename)
+                    file.write(f"Отсутствует файл {filename} - {description}\n")
+        if missing_files:
+            await bot.send_document(callback_query.from_user.id,
+                                    document=FSInputFile(f"отсутствующие_файлы_{dir}.txt"),
+                                    caption=f"Отсутствующие табеля {dir}")
+        else:
+            await bot.send_message(callback_query.from_user.id, f"Все файлы на месте для {dir}.")
 
 @router.callback_query(F.data == "checking_availability_of_reports")
 async def checking_availability_of_reports(callback_query: types.CallbackQuery):
@@ -64,3 +90,5 @@ async def checking_availability_of_reports(callback_query: types.CallbackQuery):
 
 def register_handlers_admin():
     dp.register_message_handler(checking_availability_of_reports, commands="checking_availability_of_reports")
+
+    dp.register_message_handler(checking_availability_of_tab, commands="checking_availability_of_tab")
--- keyboards/admin_keyboards/admin_keyboards.py
+++ keyboards/admin_keyboards/admin_keyboards.py
@@ -5,20 +5,18 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 def welcome_keyboard_admin():
     """Клавиатура приветствия админа"""
 
-    rows = [
+    return InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text='Зарегистрированные пользователи', callback_data='registered_users')],
         [InlineKeyboardButton(text="Активные пользователи", callback_data="active_users")],
         [InlineKeyboardButton(text='Коллективный договор', callback_data='collective_agreement')],
 
         [InlineKeyboardButton(text='Список участков', callback_data='list_of_sites')],
         [InlineKeyboardButton(text='Проверка наличия рапортов', callback_data='checking_availability_of_reports')],
+        [InlineKeyboardButton(text='Проверка наличия табеля 2024', callback_data='checking_availability_of_tab')],
         [InlineKeyboardButton(text='Переименование рапортов', callback_data='renaming_reports')],
 
         [InlineKeyboardButton(text='↩️  Вернуться в начальное меню', callback_data='menu')],
-    ]
-
-    main_keyboard = InlineKeyboardMarkup(inline_keyboard=rows)
-    return main_keyboard
+    ])
 
 
 if __name__ == '__main__':
